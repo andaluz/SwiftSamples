8ea6df0cfc60713cef8a07d63f8214a012b1bb47
Nordin Mobile Dev <lidmaatschap@gmail.com>
2015-10-01 17:17:08 +0200

    Added shake animation, ie when login failed.

--- Swifty/AnimationVC.swift
+++ Swifty/AnimationVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class AnimationVC: UIViewController {
 
@@ -26,6 +27,8 @@ class AnimationVC: UIViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
+        // dispatch is necessary otherwise, aimation won't work.
+        // maybe because at this point the floating button is not yet layoued out
         dispatch_async(dispatch_get_main_queue(), {
         self.performFloatingAnimation()
         })
@@ -33,8 +36,7 @@ class AnimationVC: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        let y = self.view.frame.size.height
-        self.floatingButton.frame = CGRectMake(self.floatingButton.frame.origin.x, y, self.floatingButton.frame.size.width, self.floatingButton.frame.size.height)
+        // This method is not necessary
     }
     
     @IBAction func didPressFloatingButton(sender: AnyObject) {
@@ -47,6 +49,9 @@ class AnimationVC: UIViewController {
         })
     }
 
+    @IBAction func didPressError(sender: AnyObject) {
+        self.shakeView(sender as! UIView)
+    }
 
     func performFloatingAnimation() {
         print("Start animating!")
@@ -57,6 +62,33 @@ class AnimationVC: UIViewController {
             }, completion: { finished in
                 print("Animation floating button finished!")
         })
+    }
+    
+    func shakeView(viewToShake:UIView) {
+        let t: CGFloat = 10.0
+        let translateRight: CGAffineTransform  = CGAffineTransformTranslate(CGAffineTransformIdentity, t, 0.0)
+        let translateLeft: CGAffineTransform = CGAffineTransformTranslate(CGAffineTransformIdentity, -t, 0.0)
+        
+        viewToShake.transform = translateLeft;
+        
+        let options: UIViewAnimationOptions = [UIViewAnimationOptions.Autoreverse,UIViewAnimationOptions.Repeat]
+        UIView.animateWithDuration(0.07, delay: 0.0, options: options , animations: {
+            UIView.setAnimationRepeatCount(4.0)
+            viewToShake.transform = translateRight
+            }, completion: { finished in
+                if (finished) {
+                    let option: UIViewAnimationOptions = UIViewAnimationOptions.BeginFromCurrentState
+                    UIView.animateWithDuration(0.05, delay: 0.0, options: option, animations: {
+                        viewToShake.transform = CGAffineTransformIdentity;
+                        }, completion: { finished in
+                            if (finished) {
+                                
+                            }
+                    })
+                }
+            })
+        
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate));
     }
     
     
